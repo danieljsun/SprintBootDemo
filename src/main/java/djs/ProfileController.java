d7872279c51f06ad880066c1a33c28b214006d2a
Daniel Sun <danieljsun@gmail.com>
2017-03-25 23:42:36 +1100

    Allow CORS from lh:3000, which is where the frontend may run

--- src/main/java/djs/ProfileController.java
+++ src/main/java/djs/ProfileController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 public class ProfileController {
 	@GetMapping(value="/profile")
